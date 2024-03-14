cf25574b13b950ab0587988e059ec22d9a9988ca
buildpeak <52726774+buildpeak@users.noreply.github.com>
2024-03-15 01:01:05 +0800

    fix bug of using exists

--- lua/gotests.lua
+++ lua/gotests.lua
@@ -4,17 +4,17 @@ local exists = fn.exists
 local M = {}
 
 local function gotests()
-  if exists(vim.g.loaded_vim_gotests) == true then
+  if exists('g:loaded_vim_gotests') == true then
     return
   end
 
   vim.g.loaded_vim_gotests = 1
 
-  if exists(vim.g.gotests_bin) == 0 or not vim.g.gotests_bin then
+  if exists('g:gotests_bin') == 0 or not vim.g.gotests_bin then
     vim.g.gotests_bin = 'gotests'
   end
 
-  if exists(vim.g.gotests_template_dir) == 0 or not vim.g.gotests_template_dir then
+  if exists('g:gotests_template_dir') == 0 or not vim.g.gotests_template_dir then
     vim.g.gotests_template_dir = ''
   end
 end
